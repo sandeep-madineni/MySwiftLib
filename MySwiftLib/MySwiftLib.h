6d980dd451be7fa5b0a67d540f7ce85fe8716ac3
smadinen <smadine@conviva.com>
2019-11-27 15:24:10 +0530

    Initial project

---
+++ MySwiftLib/MySwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  MySwiftLib.h
+//  MySwiftLib
+//
+//  Created by Sandeep Madineni on 27/11/19.
+//  Copyright © 2019 Sandeep Madineni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySwiftLib.
+FOUNDATION_EXPORT double MySwiftLibVersionNumber;
+
+//! Project version string for MySwiftLib.
+FOUNDATION_EXPORT const unsigned char MySwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySwiftLib/PublicHeader.h>
+
+
